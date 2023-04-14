3e136f9bd22d550d6eb8c92856dc31b93e5f765b
021Skyfall <021skyfall@gmail.com>
2023-04-14 12:31:40 +0900

    first commit

---
+++ Server/pre/src/main/java/com/undefined14/pre/PreApplication.java
@@ -0,0 +1,13 @@
+package com.undefined14.pre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreApplication.class, args);
+	}
+
+}
---
+++ Server/pre/src/test/java/com/undefined14/pre/PreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.undefined14.pre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
