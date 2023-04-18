2703ad2a368ecf6b7b9ef7a2f1af14575d74e727
용장 <dyswkal5@gmail.com>
2023-04-18 17:45:31 +0900

    (멤버십) MemberService updateMember 로직 작성 findVerifiedMember 로직 작성

--- Server/pre/src/main/java/com/undefined14/pre/member/service/MemberService.java
+++ Server/pre/src/main/java/com/undefined14/pre/member/service/MemberService.java
@@ -5,24 +5,34 @@ import com.undefined14.pre.member.repository.MemberRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor
 public class MemberService {
-
     private MemberRepository memberRepository;
 
     public Member createMember(Member member) {
-
         return memberRepository.save(member);
     }
 
     public Member updateMember(Member member) {
 
         Member findMember = findVerifiedMember(member.getMemberId());
-
         // TODO: 2023-04-18 내용 수정 로직 필요
+        Optional.ofNullable(member.getName())
+                .ifPresent(name -> findMember.setName(name));
+        Optional.ofNullable(member.getEmail())
+                .ifPresent(email -> findMember.setEmail(email));
+        Optional.ofNullable(member.getPassword())
+                .ifPresent(password -> findMember.setPassword(password));
+        Optional.ofNullable(member.getMemberStatus())
+                .ifPresent(memberStatus -> findMember.setMemberStatus(memberStatus));
 
-        return memberRepository.save(updatedMember);
+        findMember.setModifiedAt(LocalDateTime.now());
+
+        return memberRepository.save(findMember);
     }
 
     public Member findMember(long memberId) {
@@ -32,10 +42,15 @@ public class MemberService {
     public void deleteMember(long memberId) {
         Member findMember = findVerifiedMember(memberId);
 
-        memberRepository.delete(findmember);
+        memberRepository.delete(findMember);
     }
 
-    private Member findVerifiedMember(Member member) {
+    private Member findVerifiedMember(long memberId) {
         // TODO: 2023-04-18  DB에서 멤버 id로 멤버 찾아와야 합니다.
+        Optional<Member> optionalMember = memberRepository.findById(memberId);
+        Member findMember =
+                optionalMember.orElseThrow(() ->
+                        new BuninessLogicException(Exception.MEMBER_NOT_FOUND));
+        return findMember;
     }
 }
