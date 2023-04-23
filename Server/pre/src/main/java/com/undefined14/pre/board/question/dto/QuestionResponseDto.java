c66ebb97ffbe7cdb01d2de602e518d8c406b4ce2
용장 <dyswkal5@gmail.com>
2023-04-23 14:57:09 +0900

    (질문) QuestionResponseDto 필요없는 생성자 정리

--- Server/pre/src/main/java/com/undefined14/pre/board/question/dto/QuestionResponseDto.java
+++ Server/pre/src/main/java/com/undefined14/pre/board/question/dto/QuestionResponseDto.java
@@ -18,13 +18,4 @@ public class QuestionResponseDto {
     private LocalDateTime create_at;
     private Question.QuestionStatus questionStatus;
 
-    public QuestionResponseDto(Long questionId, Long memberId, String title, String body, LocalDateTime create_at, Question.QuestionStatus questionStatus) {
-        this.questionId = questionId;
-        this.memberId = memberId;
-        this.title = title;
-        this.body = body;
-        this.create_at = create_at;
-        this.questionStatus = questionStatus;
-    }
-
 }
