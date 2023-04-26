0b667eb50e06d72af29c853675223d316d6608c8
NoblesseCode <shb110997@gmail.com>
2023-04-26 14:01:12 +0900

    answer

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,10 @@ import Result from './Components/Result';
 import Question from './Components/Question';
 import Write from './Components/Write'
 import styled from "styled-components"
+
+import Answer from "./Components/Answer";
+import AnswerList from "./Components/AnswerList";
+import AnswerForm from "./Components/AnswerForm";
 // "proxy": "https://6a4c-175-213-102-16.ngrok-free.app/"
 
 const MainContent = styled.div`
@@ -20,21 +24,24 @@ const MainContent = styled.div`
     `;
 
 function App() {
-  
+
   const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
-  
+
   return (
     <>
       <Header />
       <MainContent>
         <Routes>
-            <Route path="/write" element={<Write />} />
-            <Route path="/question" element={<Question />} />
-            <Route path="/join" element={<Join />} />
-            <Route path="/result" element={<Result />} />
-            <Route path="/EditProfile" element={<EditProfile></EditProfile>} />
-            <Route path="/login" element={isAuthenticated ? <Navigate to="/" /> : <LoginForm />}/>
-            <Route path="/" element={isAuthenticated ? <HomePage /> : <Navigate to="/login" />}/>
+          <Route path="/write" element={<Write />} />
+          <Route path="/question" element={<Question />} />
+          <Route path="/join" element={<Join />} />
+          <Route path="/result" element={<Result />} />
+          <Route path="/EditProfile" element={<EditProfile></EditProfile>} />
+          <Route path="/login" element={isAuthenticated ? <Navigate to="/" /> : <LoginForm />} />
+          <Route path="/" element={isAuthenticated ? <HomePage /> : <Navigate to="/login" />} />
+          <Route path="/Answer" element={<Answer />} />
+          <Route path="/AnswerList" element={<AnswerList />} />
+          <Route path="/ AnswerForm" element={<AnswerForm />} />
         </Routes>
       </MainContent>
     </>
--- client/src/components/EditProfile.js
+++ client/src/components/EditProfile.js
@@ -1,211 +1,243 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import TextEdit from "./TextEdit.js";
 
 const EditProfile = () => {
-    const [profileImage, setProfileImage] = useState("");
-    const [displayName, setDisplayName] = useState("");
-    const [location, setLocation] = useState("");
-    const [title, setTitle] = useState("");
-    const [aboutMe, setAboutMe] = useState("");
-    const [websiteLink, setWebsiteLink] = useState("");
-    const [githubLink, setGithubLink] = useState("");
-    const [twitterLink, setTwitterLink] = useState("");
+  const [profileImage, setProfileImage] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [location, setLocation] = useState("");
+  const [title, setTitle] = useState("");
+  const [aboutMe, setAboutMe] = useState("");
+  const [websiteLink, setWebsiteLink] = useState("");
+  const [githubLink, setGithubLink] = useState("");
+  const [twitterLink, setTwitterLink] = useState("");
 
-    const handleCancel = () => {
-        // Cancel 버튼 클릭 시, 상태 초기화
-        setProfileImage("");
-        setDisplayName("");
-        setLocation("");
-        setTitle("");
-        setAboutMe("");
-        setWebsiteLink("");
-        setGithubLink("");
-        setTwitterLink("");
-    };
+  const handleCancel = () => {
+    // Cancel 버튼 클릭 시, 상태 초기화
+    setProfileImage("");
+    setDisplayName("");
+    setLocation("");
+    setTitle("");
+    setAboutMe("");
+    setWebsiteLink("");
+    setGithubLink("");
+    setTwitterLink("");
+  };
 
-    return (
-        <Wrapper>
-            <h1>Edit Profile</h1>
-            <ProfileImage>
-                <img src={profileImage} alt="Profile" />
-                <input
-                    type="text"
-                    placeholder="Profile Image URL"
-                    value={profileImage}
-                    onChange={(e) => setProfileImage(e.target.value)}
-                />
-            </ProfileImage>
-            <InputContainer>
-                <label>Display Name</label>
-                <input
-                    type="text"
-                    placeholder="Display Name"
-                    value={displayName}
-                    onChange={(e) => setDisplayName(e.target.value)}
-                />
-            </InputContainer>
-            <InputContainer>
-                <label>Location</label>
-                <input
-                    type="text"
-                    placeholder="Location"
-                    value={location}
-                    onChange={(e) => setLocation(e.target.value)}
-                />
-            </InputContainer>
-            <InputContainer>
-                <label>Title</label>
-                <input
-                    type="text"
-                    placeholder="Title"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                />
-            </InputContainer>
-            <InputContainer>
-                <label>About Me</label>
-                <textarea
-                    placeholder="About Me"
-                    value={aboutMe}
-                    onChange={(e) => setAboutMe(e.target.value)}
-                />
-            </InputContainer>
-            <LinkContainer>
-                <LinkInputContainer>
-                    <label>Website Link</label>
-                    <input
-                        type="text"
-                        placeholder="Website Link"
-                        value={websiteLink}
-                        onChange={(e) => setWebsiteLink(e.target.value)}
-                    />
-                </LinkInputContainer>
-                <LinkInputContainer>
-                    <label>Github Link</label>
-                    <input
-                        type="text"
-                        placeholder="Github Link"
-                        value={githubLink}
-                        onChange={(e) => setGithubLink(e.target.value)}
-                    />
-                </LinkInputContainer>
-                <LinkInputContainer>
-                    <label>Twitter Link</label>
-                    <input
-                        type="text"
-                        placeholder="Twitter Link"
-                        value={twitterLink}
-                        onChange={(e) => setTwitterLink(e.target.value)}
-                    />
-                </LinkInputContainer>
-            </LinkContainer>
-            <ButtonContainer>
-                <SaveButton>Save Profile</SaveButton>
-                <CancelButton onClick={handleCancel}>Cancel</CancelButton>
-            </ButtonContainer>
-        </Wrapper>
-    );
+  return (
+
+    <Wrapper>
+      <h1>Edit your profile</h1>
+      <p>Public information</p>
+      <BoxContainer>
+        <ProfileImage>
+          <img src={profileImage} alt="" />
+          <input
+            type="text"
+            placeholder="Profile Image URL"
+            value={profileImage}
+            onChange={(e) => setProfileImage(e.target.value)}
+          />
+        </ProfileImage>
+        <InputContainer>
+          <label>Display Name</label>
+          <input
+            type="text"
+            placeholder="Display Name"
+            value={displayName}
+            onChange={(e) => setDisplayName(e.target.value)}
+          />
+        </InputContainer>
+        <InputContainer>
+          <label>Location</label>
+          <input
+            type="text"
+            placeholder="Location"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </InputContainer>
+        <InputContainer>
+          <label>Title</label>
+          <input
+            type="text"
+            placeholder="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </InputContainer>
+        <InputContainer>
+          <label>About Me</label>
+          <TextEdit
+            placeholder="About Me"
+            value={aboutMe}
+            onChange={(e) => setAboutMe(e.target.value)}
+          />
+        </InputContainer>
+      </BoxContainer>
+
+      <p>Links</p>
+      <BoxContainer>
+
+        <LinkContainer>
+          <LinkInputContainer>
+            <label>Website Link</label>
+            <input
+              type="text"
+              placeholder="Website Link"
+              value={websiteLink}
+              onChange={(e) => setWebsiteLink(e.target.value)}
+            />
+          </LinkInputContainer>
+          <LinkInputContainer>
+            <label>Github Link</label>
+            <input
+              type="text"
+              placeholder="Github Link"
+              value={githubLink}
+              onChange={(e) => setGithubLink(e.target.value)}
+            />
+          </LinkInputContainer>
+          <LinkInputContainer>
+            <label>Twitter Link</label>
+            <input
+              type="text"
+              placeholder="Twitter Link"
+              value={twitterLink}
+              onChange={(e) => setTwitterLink(e.target.value)}
+            />
+          </LinkInputContainer>
+        </LinkContainer>
+      </BoxContainer>
+
+      <BoxContainer>
+
+        <ButtonContainer>
+          <SaveButton>Save Profile</SaveButton>
+          <CancelButton onClick={handleCancel}>Cancel</CancelButton>
+        </ButtonContainer>
+      </BoxContainer>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    /* align-items: center; */
-    padding: 100px;
-  `;
+      display: flex;
+      flex-direction: column;
+      /* align-items: center; */
+      padding: 100px;
+      p {
+        font-size : 25px;
+      }
+      `;
+
+const BoxContainer = styled.div`
+      display: flex;
+      flex-direction: column;
+      padding: 30px 20px;
+      border : 1px; solid #ccc;
+      p {
+        margin-top: 60px;
+        padding-top: 20px;
+        font-size : 20px;
+      }
+      `
 
 const ProfileImage = styled.div`
-  margin-top: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  img {
-    width: 200px;
-    height: 200px;
-    object-fit: cover;
-    border-radius: 50%;
-    margin-bottom: 10px;
+      margin-top: 20px;
+      display: flex;
+      flex-direction: column;
+      align-items: right;
+      img {
+        width: 200px;
+      height: 200px;
+      object-fit: cover;
+      border-radius: 50%;
+      margin-bottom: 10px;
   }
-  input {
-    margin-top: 10px;
+      input {
+        margin - top: 10px;
   }
-`;
+      `;
 
 const InputContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-top: 20px;
-  width: 20%;
-  label {
-    margin-bottom: 5px;
+      display: flex;
+      flex-direction: column;
+      margin-top: 20px;
+      width: 40%;
+      label {
+        margin - bottom: 5px;
   }
-  input,
-  textarea {
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    padding: 10px;
-    margin-bottom: 10px;
-    font-size: 16px;
-    width: 100%;
+      input,
+      textarea {
+        border: 1px solid #ccc;
+      border-radius: 5px;
+      padding: 10px;
+      margin-bottom: 10px;
+      font-size: 16px;
+      width: 60%;
   }
-`;
+      TextEdit {
+
+      }
+      `;
 
 const LinkContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  margin-top: 20px;
-  width: 30%;
-  justify-content: space-between;
-`;
+      display: flex;
+      flex-direction: row;
+      /* margin-top: 20px; */
+      width: 40%;
+      justify-content: space-between;
+      `;
 
 const LinkInputContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 30%;
-  label {
-    margin-bottom: 5px;
+      display: flex;
+      flex-direction: column;
+      width: 25%;
+      label {
+        margin - bottom: 5px;
   }
-  input {
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    padding: 10px;
-    margin-bottom: 10px;
-    font-size: 16px;
-    width: 100%;
+      input {
+        border: 1px solid #ccc;
+      border-radius: 5px;
+      padding: 10px;
+      margin-bottom: 10px;
+      font-size: 16px;
+      width: 100%;
   }
-`;
+      `;
 
 const ButtonContainer = styled.div`
-  display: flex;
-  margin-top: 20px;
-`;
+      display: flex;
+      margin-top: 20px;
+      `;
 
 const SaveButton = styled.button`
-  background-color: #0a95ff;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  padding: 10px 20px;
-  font-size: 16px;
-  margin-right: 10px;
-  cursor: pointer;
-  &:hover {
-    background-color: #0074cc;
+      background-color: #0a95ff;
+      color: white;
+      border: none;
+      border-radius: 5px;
+      padding: 10px 20px;
+      font-size: 16px;
+      margin-right: 10px;
+      cursor: pointer;
+      &:hover {
+        background - color: #0074cc;
   }
-`;
+      `;
 
 const CancelButton = styled.button`
-  background-color: #ccc;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  padding: 10px 20px;
-  font-size: 16px;
-  cursor: pointer;
-  &:hover {
-    background-color: #999;
+      background-color: #ccc;
+      color: white;
+      border: none;
+      border-radius: 5px;
+      padding: 10px 20px;
+      font-size: 16px;
+      cursor: pointer;
+      &:hover {
+        background - color: #999;
   }
-`;
+      `;
 
 
 // const ProfileImage = styled.div`
