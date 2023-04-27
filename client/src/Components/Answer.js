5aaf649ef8b10cd10431a55b950a5a5c09b45828
NoblesseCode <shb110997@gmail.com>
2023-04-27 10:20:14 +0900

    Answer CRUD

--- client/src/Components/Answer.js
+++ client/src/Components/Answer.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import TextEdit from './TextEdit';
+import SubmittedAnswer from './SubmittedAnswer';
 const AnswerWrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -8,9 +9,7 @@ const AnswerWrapper = styled.div`
   padding: 16px;
 `;
 
-const AnswerText = styled.p`
-  margin: 0;
-`;
+
 
 const AnswerButtonWrapper = styled.div`
   display: flex;
@@ -20,33 +19,67 @@ const AnswerButtonWrapper = styled.div`
 
 const AnswerButton = styled.button`
   padding: 8px;
-  background-color: ${(props) => props.color};
+  background-color: #0a95ff;
   color: #fff;
-  border: none;
+  border: 1px solid #0a95ff;
+  border-radius: 3px;
+  box-shadow: rgba(255, 255, 255, 0.5) 0px 1px inset;
   cursor: pointer;
   &:hover {
-    opacity: 0.8;
+    background-color: #0074cc;
   }
 `;
 
-const Answer = ({ id, text, onDelete }) => {
-  const handleDeleteClick = () => {
-    onDelete(id);
+const ClearButton = styled.button`
+  padding: 8px;
+  background-color: #fff;
+  color: #0a95ff;
+  border: 1px solid #0a95ff;
+  border-radius: 3px;
+  box-shadow: rgba(255, 255, 255, 0.5) 0px 1px inset;
+  cursor: pointer;
+  &:hover {
+    background-color: #0074cc;
+    color: #fff;
+  }
+`;
+
+
+const Answer = () => {
+  const [answer, setAnswer] = useState('');
+  const [submittedAnswer, setSubmittedAnswer] = useState('');
+
+  const handleAnswerChange = (value) => {
+    setAnswer(value);
+  };
+
+  const handleClear = () => {
+    setAnswer('');
+  };
+
+
+
+  const handleSubmit = () => {
+    // 답변 작성을 완료하고, 작성된 답변을 저장하거나 다른 작업을 수행하는 함수
+    setSubmittedAnswer(answer);
+    setAnswer('');
+
+    console.log(answer);
+  };
+
+  const handleDelete = () => {
+    setSubmittedAnswer('');
   };
 
   return (
     <AnswerWrapper>
-      <AnswerText>{text}</AnswerText>
-      <TextEdit
-
-      />
+      <TextEdit value={answer} handleChange={handleAnswerChange} />
       <AnswerButtonWrapper>
-        <AnswerButton color="#0077cc">Post Your Answer</AnswerButton>
-        {/* <AnswerButton color="#cc0000" onClick={handleDeleteClick}>
+        <AnswerButton onClick={handleSubmit} >Post Your Answer</AnswerButton>
+        <ClearButton onClick={handleClear}>Clear</ClearButton>
 
-          Delete
-        </AnswerButton> */}
       </AnswerButtonWrapper>
+      {submittedAnswer && <SubmittedAnswer answer={submittedAnswer} handleDelete={handleDelete} />}
     </AnswerWrapper>
   );
 };
---
+++ client/src/Components/SubmittedAnswer.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+import 'react-quill/dist/quill.snow.css';
+
+const AnswerWrapper = styled.div`
+  border: 1px solid #ccc;
+  padding: 16px;
+  margin-top: 16px;
+`;
+
+const AnswerTitle = styled.h3`
+  margin-top: 0;
+`;
+
+const SubmittedAnswer = ({ answer }) => {
+  return (
+    <AnswerWrapper>
+      <AnswerTitle>Submitted Answer:</AnswerTitle>
+      <div dangerouslySetInnerHTML={{ __html: answer }} />
+    </AnswerWrapper>
+  );
+};
+
+export default SubmittedAnswer;
